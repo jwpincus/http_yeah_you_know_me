ef9bc804a47f43087f8a9b8b407013ad2d8d4ca9
Jack Pincus <jwpincus@gmail.com>
2017-02-11 10:31:16 -0700

    hello world refactor

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -1,15 +1,24 @@
 require 'socket' # Provides TCPServer and TCPSocket classes
+class HelloServer
+  def self.start_server
+    server = TCPServer.new(9292)
+    counter = 1
+    server_run(server, counter)
+    
+  end
 
+  def self.server_run(server, counter)
+    loop do
+      socket = server.accept
+      request = socket.gets
+      response = "Hello World!\n (#{counter})"
+      socket.print "\r\n"
+      socket.print response
+      socket.close
+      counter += 1
+    end
+  end
 
-server = TCPServer.new(9292)
-counter = 1
-
-loop do
-  socket = server.accept
-  request = socket.gets
-  response = "Hello World!\n (#{counter})"
-  socket.print "\r\n"
-  socket.print response
-  socket.close
-  counter += 1
 end
+
+HelloServer.start_server
